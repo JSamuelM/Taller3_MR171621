1a426919e97e2d14435fd460873b37bcaad13b6f
Samuel Mena <samuel23mena@gmail.com>
2020-11-18 11:38:34 -0600

    Cambios pequeños en el componente y template de app

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Taller3MR171621
+# Taller 3 - MR171621
+
+José Samuel Mena Reyes - 01L
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.2.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { Persona } from './models/persona';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Taller3-MR171621';
+
+  personas: Persona[] = [
+    { id: 1, nombre: 'Ricardo', apellido: 'Moya', edad: 28 },
+    { id: 2, nombre: 'Ramón', apellido: 'Invarato', edad: 28 },
+    { id: 3, nombre: 'Pepe', apellido: 'Moreno', edad: 40 },
+    { id: 4, nombre: 'Paco Pepe', apellido: 'Gonzalez', edad: 32 },
+  ];
+
+  // Para seleccionar una persona de tipo Persona y almacenar un objeto
+  selectPersona: Persona = { id: 0, nombre: '', apellido: '', edad: 0 };
+
+  // Recibe como parametro una variable de tipo Persona, para ser almacenada en selectPersona
+  selectedPersona(persona: Persona) {
+    this.selectPersona = persona;
+    console.log(persona);
+  }
+
+  // Método para guardar persona o seleccionar una
+  agregarEditarPersona() {
+    // Guardara una persona si no se ha seleccionado nada aún
+    if (this.selectPersona.id === 0) {
+      this.selectPersona.id = this.personas.length + 1;
+      this.personas.push(this.selectPersona);
+    }
+    this.selectPersona = { id: 0, nombre: '', apellido: '', edad: 0 };
+  }
+
+  // Método para eliminar una persona
+  eliminarPersona() {
+    if (confirm('¿Esta seguro de eliminar el Registro?')) {
+      // metodo filter, nos permite filtrar elementos de un array
+      this.personas = this.personas.filter(
+        (person) => person != this.selectPersona
+      );
+      this.selectPersona = { id: 0, nombre: '', apellido: '', edad: 0 };
+    }
+  }
 }
